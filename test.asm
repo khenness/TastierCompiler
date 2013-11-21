4cc36f967a53574ca2a5b8d2ab3fbfe65d83ef56
khenness <khenness@tcd.ie>
2013-11-21 23:02:12 +0000

    have the first part of the question working, up to the const bit

--- test.asm
+++ test.asm
@@ -1,53 +1,38 @@
-.names 4
+.names 1
 .proc Main
-.proc SumUp
-.var 2 b
-.var 1 i
-SumUp: Enter 2
-Const 0
-Sto 0 0
-L$0: Nop
-LoadG 3
-Const 0
-Gtr
-FJmp L$1
-Load 0 0
-LoadG 3
-Add
-Sto 0 0
-LoadG 3
+Main: Enter 3
 Const 1
-Sub
-StoG 3
-Jmp L$0
-L$1: Nop
-Load 0 0
-Write
-Const 9999
+Sto 0 0
+Const 2
 Sto 0 1
-L$2: Nop
-Load 0 1
+Const 3
+Sto 0 2
 Const 1
-Equ
-FJmp L$3
+Const 0
+Neq
+FJmp L$0
+Load 0 0
+Write
+Jmp L$1
+L$0: Nop
+L$1: Nop
+Const 1
+Const 1
+Leq
+FJmp L$2
 Load 0 1
 Write
-Jmp L$2
+Jmp L$3
+L$2: Nop
 L$3: Nop
-Leave
-Ret
-Main: Enter 0
-Read
-StoG 3
+Const 1
+Const 1
+Geq
+FJmp L$4
+Load 0 2
+Write
+Jmp L$5
 L$4: Nop
-LoadG 3
-Const 0
-Gtr
-FJmp L$5
-Call 1 SumUp
-Read
-StoG 3
-Jmp L$4
 L$5: Nop
 Leave
 Ret

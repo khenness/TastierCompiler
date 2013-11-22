1e40c6a89cc6b1ded9ee2a27ab52e6e08e57492e
khenness <khenness@tcd.ie>
2013-11-22 21:12:41 +0000

    about to submit exercise 5

--- test.asm
+++ test.asm
@@ -1,7 +1,11 @@
-.names 2
+.names 5
 .proc Main
-Const 1
+.var 1 d
+.var 1 c
+Const 6
 StoG 3
+Const 9
+StoG 4
 Main: Enter 3
 Const 1
 Sto 0 0
@@ -9,7 +13,40 @@ Const 2
 Sto 0 1
 Const 3
 Sto 0 2
+Const 1
+Const 0
+Neq
+FJmp L$0
 Load 0 0
+Write
+Jmp L$1
+L$0: Nop
+L$1: Nop
+Const 1
+Const 1
+Leq
+FJmp L$2
+Load 0 1
+Write
+Jmp L$3
+L$2: Nop
+L$3: Nop
+Const 1
+Const 1
+Geq
+FJmp L$4
+Load 0 2
+Write
+Jmp L$5
+L$4: Nop
+L$5: Nop
+Const 4
+StoG 5
+Const 5
+StoG 6
+LoadG 5
+Write
+LoadG 6
 Write
 Leave
 Ret

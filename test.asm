5a52ba696ac8d98c01d28824a95ead09642bd33b
khenness <khenness@tcd.ie>
2013-11-22 10:30:29 +0000

    added Fintans changes

--- test.asm
+++ test.asm
@@ -1,39 +1,11 @@
 .names 2
 .proc Main
-.var 1 a
-Main: Enter 3
 Const 1
+StoG 3
+Main: Enter 1
+Const 99
 Sto 0 0
-Const 2
-Sto 0 1
-Const 3
-Sto 0 2
-Const 1
-Const 0
-Neq
-FJmp L$0
 Load 0 0
 Write
-Jmp L$1
-L$0: Nop
-L$1: Nop
-Const 1
-Const 1
-Leq
-FJmp L$2
-Load 0 1
-Write
-Jmp L$3
-L$2: Nop
-L$3: Nop
-Const 1
-Const 1
-Geq
-FJmp L$4
-Load 0 2
-Write
-Jmp L$5
-L$4: Nop
-L$5: Nop
 Leave
 Ret

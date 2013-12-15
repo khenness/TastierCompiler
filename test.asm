cece262662d14ebbb3a48432d4530ca5b56209f2
khenness <khenness@tcd.ie>
2013-12-15 17:01:53 +0000

    stable exercise 6

--- test.asm
+++ test.asm
@@ -19,6 +19,23 @@ Write
 L$0: Nop
 Jmp L$1
 L$2: Nop
+Const 10
+Sto 0 0
+Jmp L$3
+L$4: Nop
+Load 0 0
+Const 0
+Gtr
+Load 0 0
+Const 1
+Sub
+Sto 0 0
+FJmp L$5
+Load 0 0
+Write
+L$3: Nop
+Jmp L$4
+L$5: Nop
 Const 111
 Sto 0 1
 Const 999
@@ -26,27 +43,27 @@ Sto 0 2
 Const 2
 Const 1
 Gtr
-FJmp L$3
+FJmp L$6
 Load 0 1
 Sto 0 3
-Jmp L$4
-L$3: Nop
+Jmp L$7
+L$6: Nop
 Load 0 2
 Sto 0 3
-L$4: Nop
+L$7: Nop
 Load 0 3
 Write
 Const 2
 Const 1
 Lss
-FJmp L$5
+FJmp L$8
 Load 0 1
 Sto 0 3
-Jmp L$6
-L$5: Nop
+Jmp L$9
+L$8: Nop
 Load 0 2
 Sto 0 3
-L$6: Nop
+L$9: Nop
 Load 0 3
 Write
 Leave

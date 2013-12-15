21d9eefbc9ed94d4b0b10fbc6bb0fe21371eea32
khenness <khenness@tcd.ie>
2013-12-15 17:43:07 +0000

    ??

--- test.asm
+++ test.asm
@@ -1,8 +1,21 @@
+<<<<<<< HEAD
 .names 2
 .proc Main
 .proc ex6Test
 ex6Test: Enter 4
 Const 0
+=======
+.names 5
+.proc Main
+.var 1 d
+.var 1 c
+Const 6
+StoG 3
+Const 9
+StoG 4
+Main: Enter 3
+Const 1
+>>>>>>> b52d399dbb3c7a960831cb8a228f7743c2cc21e5
 Sto 0 0
 Jmp L$0
 L$1: Nop
@@ -19,23 +32,6 @@ Write
 L$0: Nop
 Jmp L$1
 L$2: Nop
-Const 10
-Sto 0 0
-Jmp L$3
-L$4: Nop
-Load 0 0
-Const 0
-Gtr
-Load 0 0
-Const 1
-Sub
-Sto 0 0
-FJmp L$5
-Load 0 0
-Write
-L$3: Nop
-Jmp L$4
-L$5: Nop
 Const 111
 Sto 0 1
 Const 999
@@ -43,32 +39,46 @@ Sto 0 2
 Const 2
 Const 1
 Gtr
-FJmp L$6
+FJmp L$3
 Load 0 1
 Sto 0 3
-Jmp L$7
-L$6: Nop
+Jmp L$4
+L$3: Nop
 Load 0 2
 Sto 0 3
-L$7: Nop
+L$4: Nop
 Load 0 3
 Write
 Const 2
 Const 1
 Lss
-FJmp L$8
+FJmp L$5
 Load 0 1
 Sto 0 3
-Jmp L$9
-L$8: Nop
+Jmp L$6
+L$5: Nop
 Load 0 2
 Sto 0 3
-L$9: Nop
+L$6: Nop
 Load 0 3
 Write
+<<<<<<< HEAD
 Leave
 Ret
 Main: Enter 0
 Call 1 ex6Test
+=======
+Jmp L$5
+L$4: Nop
+L$5: Nop
+Const 4
+StoG 5
+Const 5
+StoG 6
+LoadG 5
+Write
+LoadG 6
+Write
+>>>>>>> b52d399dbb3c7a960831cb8a228f7743c2cc21e5
 Leave
 Ret

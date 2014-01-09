f2ab5e32f752531a128c2a839ae341b8857b2225
khenness <khenness@tcd.ie>
2014-01-09 20:05:02 +0000

    hunting down bugs

--- test.asm
+++ test.asm
@@ -1,21 +1,25 @@
 .names 1
 .proc Main
-Main: Enter 41
-Const 21
-Sto 0 31
-Load 0 31
+Main: Enter 26
+Const 88
+Sto 0 10
+Load 0 10
 Write
-Load 0 31
+Const 21
+Sto 0 16
+Load 0 16
+Write
+Load 0 16
 Const 21
 Add
-Sto 0 31
-Load 0 31
+Sto 0 16
+Load 0 16
 Write
-Load 0 31
+Load 0 16
 Const 1
 Sub
-Sto 0 36
-Load 0 36
+Sto 0 21
+Load 0 21
 Write
 Leave
 Ret

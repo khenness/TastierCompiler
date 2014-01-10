695f08ebb5d9474cd6a267581982c2fe915c52a0
khenness <khenness@tcd.ie>
2014-01-10 22:37:35 +0000

    added report

--- test.asm
+++ test.asm
@@ -1,29 +1,51 @@
-.names 1
+.names 2
 .proc Main
-Main: Enter 46
-Const 22
-Sto 0 32
-Const 88
-Sto 0 43
-Const 77
-Sto 0 43
-Load 0 43
-Sto 0 44
-Const 21
-Sto 0 34
-Load 0 34
+.proc myfun
+myfun: Enter 1
+Load 0 0
 Write
-Load 0 34
+Leave
+Ret
+Main: Enter 60
 Const 21
-Add
-Sto 0 34
-Load 0 34
-Write
-Load 0 34
+Sto 0 0
+Load 0 0
+Const 0
+Equ
+FJmp L$1
+Const 101
+Sto 0 1
+Jmp L$0
+L$1: Nop
 Const 1
-Sub
-Sto 0 39
-Load 0 39
+Equ
+FJmp L$2
+Const 102
+Sto 0 1
+Jmp L$0
+L$2: Nop
+Const 103
+Sto 0 1
+L$0: Nop
+Load 0 1
 Write
+Const 22
+Load 0 40
+Add
+Sto 0 6
+Load 0 6
+Write
+Const 32
+Sto 0 2
+Load 0 2
+Load 0 2
+Add
+Sto 0 2
+Load 0 2
+Write
+Const 41
+Sto 0 1
+Load 0 1
+Call 1 myfun
 Leave
 Ret
